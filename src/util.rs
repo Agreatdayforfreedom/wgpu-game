3f2ca842cc0a8711bd18c436f3d50feb55599ed0
Agreatdayforfreedom <matias.bertoni2002@gmail.com>
2024-07-21 13:10:27 -0300

    refactor: avoid dry in compassdir struct

--- src/util.rs
+++ src/util.rs
@@ -9,22 +9,17 @@ pub struct CompassDir {
 
 impl CompassDir {
     pub fn from_deg(angle: f32) -> Self {
-        let mut angle = cgmath::Deg(90.0 - angle);
-        let dir = cgmath::Vector2 {
-            x: angle.cos(),
-            y: angle.sin(),
-        }
-        .normalize();
-
-        if angle < cgmath::Deg(0.0) {
-            angle += cgmath::Deg(360.0);
-        }
-
-        Self { dir, angle }
+        let angle = cgmath::Deg(90.0 - angle);
+        Self::from_angle(angle)
     }
-    //todo
+
     pub fn rotate(&self, angle: f32) -> Self {
-        let mut angle = self.angle + cgmath::Deg(angle);
+        let angle = self.angle + cgmath::Deg(angle);
+        Self::from_angle(angle)
+    }
+
+    #[inline]
+    fn from_angle(mut angle: cgmath::Deg<f32>) -> Self {
         let dir = cgmath::Vector2 {
             x: angle.cos(),
             y: angle.sin(),
